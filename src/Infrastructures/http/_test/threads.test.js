6517551ab475993e52f3d9aaa7c68bae50d526c4
nafifurqon <muhammadnafi98@gmail.com>
2022-04-24 18:36:13 +0700

    feat: kriteria 1 - menambahkan thread

---
+++ src/Infrastructures/http/_test/threads.test.js
@@ -0,0 +1,197 @@
+const pool = require('../../database/postgres/pool');
+const AuthenticationsTableTestHelper = require('../../../../tests/AuthenticationsTableTestHelper');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
+const createServer = require('../createServer');
+const container = require('../../container');
+
+describe('/threads endpoint', () => {
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  afterEach(async () => {
+    await UsersTableTestHelper.cleanTable();
+    await AuthenticationsTableTestHelper.cleanTable();
+    await ThreadsTableTestHelper.cleanTable();
+  });
+
+  describe('when POST /threads', () => {
+    it('should response 401 when request without authorization token', async () => {
+      // Arrange
+      const requestPayload = {
+        title: 'Judul Thread',
+        body: 'Body thread.',
+      };
+
+      const server = await createServer(container);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(401);
+      expect(responseJson.message).toBeDefined();
+      expect(responseJson.message).toEqual('Missing authentication');
+    });
+
+    it('should response 400 when request payload not contain needed property', async () => {
+      // Arrange
+      const requestPayload = {
+        title: 'Judul Thread',
+      };
+
+      const server = await createServer(container);
+
+      // add user
+      await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: {
+          username: 'dicoding',
+          password: 'secret',
+          fullname: 'Dicoding Indonesia',
+        },
+      });
+
+      // login user
+      const loginResponse = await server.inject({
+        method: 'POST',
+        url: '/authentications',
+        payload: {
+          username: 'dicoding',
+          password: 'secret',
+        },
+      });
+      const { data: { accessToken } } = JSON.parse(loginResponse.payload);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('tidak dapat menambah thread karena properti yang dibutuhkan tidak ada');
+    });
+
+    it('should response 400 when request payload not meet data type specification', async () => {
+      // Arrange
+      const requestPayload = {
+        title: 'Judul Thread',
+        body: 12345,
+      };
+
+      const server = await createServer(container);
+
+      // add user
+      await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: {
+          username: 'dicoding',
+          password: 'secret',
+          fullname: 'Dicoding Indonesia',
+        },
+      });
+
+      // login user
+      const loginResponse = await server.inject({
+        method: 'POST',
+        url: '/authentications',
+        payload: {
+          username: 'dicoding',
+          password: 'secret',
+        },
+      });
+      const { data: { accessToken } } = JSON.parse(loginResponse.payload);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('tidak dapat menambah thread karena tipe data tidak sesuai');
+    });
+
+    it('should response 201 and add new thread', async () => {
+      // Arrange
+      const requestPayload = {
+        title: 'Judul Thread',
+        body: 'Body thread.',
+      };
+
+      const server = await createServer(container);
+
+      // add user
+      await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: {
+          username: 'dicoding',
+          password: 'secret',
+          fullname: 'Dicoding Indonesia',
+        },
+      });
+
+      // login user
+      const loginResponse = await server.inject({
+        method: 'POST',
+        url: '/authentications',
+        payload: {
+          username: 'dicoding',
+          password: 'secret',
+        },
+      });
+      const { data: { accessToken } } = JSON.parse(loginResponse.payload);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(201);
+      expect(responseJson.status).toEqual('success');
+      expect(responseJson.data.addedThread).toBeDefined();
+
+      const { addedThread } = responseJson.data;
+      expect(addedThread.id).toBeDefined();
+      expect(addedThread.id).not.toEqual('');
+      expect(typeof addedThread.id).toEqual('string');
+      expect(addedThread.title).toBeDefined();
+      expect(addedThread.title).not.toEqual('');
+      expect(typeof addedThread.title).toEqual('string');
+      expect(addedThread.owner).toBeDefined();
+      expect(addedThread.owner).not.toEqual('');
+      expect(typeof addedThread.owner).toEqual('string');
+    });
+  });
+});
---
+++ src/Infrastructures/repository/_test/ThreadRepositoryPostgres.test.js
@@ -0,0 +1,40 @@
+const ThreadsTableHelper = require('../../../../tests/ThreadsTableTestHelper');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const NewThread = require('../../../Domains/threads/entities/NewThread');
+const pool = require('../../database/postgres/pool');
+const ThreadRepositoryPostgres = require('../ThreadRepositoryPostgres');
+
+describe('ThreadRepositoryPostgres', () => {
+  afterEach(async () => {
+    await ThreadsTableHelper.cleanTable();
+    await UsersTableTestHelper.cleanTable();
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  describe('addThread function', () => {
+    it('should persist new thread and return added thread correctly', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({});
+      const user = await UsersTableTestHelper.findUsersById('user-123');
+
+      const newThread = new NewThread({
+        title: 'Judul',
+        body: 'Body',
+        owner: user[0].id,
+      });
+
+      const fakeIdGenerator = () => '123';
+      const threadRepositoryPostgres = new ThreadRepositoryPostgres(pool, fakeIdGenerator);
+
+      // Action
+      await threadRepositoryPostgres.addThread(newThread);
+
+      // Assert
+      const threads = await ThreadsTableHelper.findThreadsById('thread-123');
+      expect(threads).toHaveLength(1);
+    });
+  });
+});
